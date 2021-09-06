f39964bed662c31424a3a7845aaed420fc409f97
AfonSir <afonsir17@gmail.com>
2021-09-06 18:27:33 -0300

    feat: add tab navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 import AppLoading from 'expo-app-loading'
 import { ThemeProvider } from 'styled-components'
+
+import { NavigationContainer } from '@react-navigation/native'
 
 import {
   useFonts,
@@ -11,7 +14,7 @@ import {
 
 import theme from './src/global/styles/theme'
 
-import { Dashboard } from './src/screens/Dashboard'
+import { AppRoutes } from './src/routes/app.routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +29,14 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <StatusBar
+        backgroundColor={theme.colors.primary}
+        barStyle="light-content"
+      />
+
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
