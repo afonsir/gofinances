80737dc5dd5f93552c97d8e9bfeaf28fe1641f56
AfonSir <afonsir17@gmail.com>
2021-09-07 17:36:19 -0300

    feat: add async storage persistence and navigation

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -9,7 +9,11 @@ import {
 
 import * as Yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import uuid from 'react-native-uuid'
+
 import { useForm } from 'react-hook-form'
+import { useNavigation } from '@react-navigation/native'
 
 import { InputForm } from '../../components/Form/InputForm'
 import { Button } from '../../components/Form/Button'
@@ -52,9 +56,12 @@ export function Register() {
     name: 'Categoria'
   })
 
+  const navigation = useNavigation()
+
   const {
     control,
     handleSubmit,
+    reset,
     formState: { errors }
   } = useForm({
     resolver: yupResolver(schema)
@@ -72,21 +79,47 @@ export function Register() {
     setCategoryModalOpen(false)
   }
 
-  function handleRegister(form: FormData) {
+  async function handleRegister(form: FormData) {
     if (!transactionType)
       return Alert.alert('Selecione o tipo da transação')
 
     if (category.key === 'category')
       return Alert.alert('Selecione a categoria')
 
-    const data = {
+    const newTransaction = {
+      id: String(uuid.v4()),
       name: form.name,
       amount: form.amount,
-      transactionType,
-      category: category.key
+      type: transactionType,
+      category: category.key,
+      date: new Date()
     }
 
-    console.log(data)
+    try {
+      const dataKey = '@gofinances:transactions'
+      const data = await AsyncStorage.getItem(dataKey)
+      const currentData = data ? JSON.parse(data) : []
+
+      const dataFormatted = [
+        ...currentData,
+        newTransaction
+      ]
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted))
+
+      reset()
+      setTransactionType('')
+      setCategory({
+        key: 'category',
+        name: 'Categoria'
+      })
+
+      navigation.navigate("Listagem")
+
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Não foi possível salvar")
+    }
   }
 
   return (
